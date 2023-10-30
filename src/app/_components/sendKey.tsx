0c07135141b4fa03f9818bd767844e366f740f98
Vincent Udén <vincent.uden@colmeo.se>
2023-10-30 21:37:34 +0100

    Shadcn + design

--- src/app/_components/sendKey.tsx
+++ src/app/_components/sendKey.tsx
@@ -1,22 +1,78 @@
-"use client"
+"use client";
 
 import { useState } from "react";
+import { WidthProvider, Responsive } from "react-grid-layout";
+import { Toggle } from "./ui/toggle";
+import { cn } from "~/lib/utils";
+import { Input } from "./ui/input";
+import { Switch } from "./ui/switch";
+import { Label } from "./ui/label";
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
 export function SendKey() {
   const [ip, setIp] = useState("");
+  const [gridItems, setGridItems] = useState([
+    { name: "Pen" },
+    { name: "Eraser" },
+  ]);
+  const [layouts, setLayouts] = useState<any>();
+  const [editMode, setEditMode] = useState(false);
 
   async function postLocal() {
     console.log(ip);
-    await fetch(`https://${ip}:3001/press/E`, {headers:{
+    await fetch(`https://${ip}:3001/press/E`, {
+      headers: {
         "Access-Control-Request-Private-Network": "true",
-    }});
+      },
+    });
   }
-    return (
-        <>
-        <input className="text-black" type="text" value={ip} onChange={(e) => setIp(e.target.value)} />
-        <div className="bg-white/20 hover:bg-white/30 p-4 rounded-full transition-colors cursor-pointer" onClick={postLocal}>
-            <p>Send Key!</p>
+  return (
+    <>
+      <div className="flex h-full flex-col">
+        <div className="flex grow-0 flex-row gap-4 p-2 items-center">
+          <Label htmlFor="ipAddress" className="text-white whitespace-nowrap">
+            Local Server Ip
+          </Label>
+          <Input
+            className="text-white"
+            type="text"
+            id="ipAddress"
+            value={ip}
+            onChange={(e) => setIp(e.target.value)}
+          />
+          <div className="flex items-center space-x-2">
+            <Switch id="editMode" onClick={() => setEditMode(!editMode)} />
+            <Label htmlFor="editMode" className="text-white whitespace-nowrap">
+              Edit Mode
+            </Label>
+          </div>
         </div>
-        </>
-    )
+        <div className="grow select-none">
+          <ResponsiveReactGridLayout
+            cols={{ lg: 4, md: 2, sm: 2, xs: 2, xxs: 2 }}
+            preventCollision={true}
+            isResizable={editMode}
+            isDraggable={editMode}
+            layouts={layouts}
+            onLayoutChange={setLayouts}
+          >
+            {gridItems.map((item, i) => (
+              <div
+                className={cn(
+                  "flex h-full w-full flex-row items-center justify-center rounded border-slate-700 border text-blue-500 font-bold transition-colors",
+                  {
+                    "cursor-move hover:border-slate-600": editMode,
+                    "cursor-pointer": !editMode,
+                  },
+                )}
+                key={item.name}
+              >
+                {item.name}
+              </div>
+            ))}
+          </ResponsiveReactGridLayout>
+        </div>
+      </div>
+    </>
+  );
 }
