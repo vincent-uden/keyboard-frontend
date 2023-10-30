e3236eda637ff76fccdba9f527cf4eaa81b417ed
Vincent Udén <vincent.uden@colmeo.se>
2023-10-30 21:57:55 +0100

    Addtional dialogue

--- src/app/_components/sendKey.tsx
+++ src/app/_components/sendKey.tsx
@@ -7,6 +7,8 @@ import { cn } from "~/lib/utils";
 import { Input } from "./ui/input";
 import { Switch } from "./ui/switch";
 import { Label } from "./ui/label";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { ScrollArea } from "./ui/scroll-area";
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
 export function SendKey() {
@@ -29,8 +31,8 @@ export function SendKey() {
   return (
     <>
       <div className="flex h-full flex-col">
-        <div className="flex grow-0 flex-row gap-4 p-2 items-center">
-          <Label htmlFor="ipAddress" className="text-white whitespace-nowrap">
+        <div className="flex grow-0 flex-row items-center gap-4 p-2">
+          <Label htmlFor="ipAddress" className="whitespace-nowrap text-white">
             Local Server Ip
           </Label>
           <Input
@@ -42,10 +44,43 @@ export function SendKey() {
           />
           <div className="flex items-center space-x-2">
             <Switch id="editMode" onClick={() => setEditMode(!editMode)} />
-            <Label htmlFor="editMode" className="text-white whitespace-nowrap">
+            <Label htmlFor="editMode" className="whitespace-nowrap text-white">
               Edit Mode
             </Label>
           </div>
+        </div>
+        <div className={cn("flex grow-0 flex-row items-center gap-4 p-2", {"hidden": !editMode})}>
+          <Label htmlFor="" className="whitespace-nowrap text-white">
+            Icon
+          </Label>
+          <Popover>
+            <PopoverTrigger className="text-white">Open</PopoverTrigger>
+            <PopoverContent>
+              <ScrollArea className="h-32">
+                <p>Pen</p>
+                <p>Eraser</p>
+                <p>Brush</p>
+                <p>Redo</p>
+                <p>Undo</p>
+                <p>Pen</p>
+                <p>Eraser</p>
+                <p>Brush</p>
+                <p>Redo</p>
+                <p>Undo</p>
+              </ScrollArea>
+            </PopoverContent>
+          </Popover>
+
+          <Label htmlFor="newBtnName" className="whitespace-nowrap text-white">
+            Name
+          </Label>
+          <Input
+            className="text-white"
+            type="text"
+            id="newBtnName"
+            value={ip}
+            onChange={(e) => setIp(e.target.value)}
+          />
         </div>
         <div className="grow select-none">
           <ResponsiveReactGridLayout
@@ -59,7 +94,7 @@ export function SendKey() {
             {gridItems.map((item, i) => (
               <div
                 className={cn(
-                  "flex h-full w-full flex-row items-center justify-center rounded border-slate-700 border text-blue-500 font-bold transition-colors",
+                  "flex h-full w-full flex-row items-center justify-center rounded border border-slate-700 font-bold text-blue-500 transition-colors",
                   {
                     "cursor-move hover:border-slate-600": editMode,
                     "cursor-pointer": !editMode,
